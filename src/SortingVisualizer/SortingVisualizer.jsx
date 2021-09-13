c8c2c8b3de70841c0db32dc1eb6afe0957358d85
arushkakkar <arushkakr@gmail.com>
2021-09-13 15:53:54 -0700

    bars gui

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './SortingVisualizer.css'
+
+export default class SortingVisualizer extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            array: []
+        };
+    }
+
+    componentDidMount(){
+        this.resetArray();
+    }
+
+    resetArray(){
+        const array = [];
+        for(let i = 0; i < 230; i++){
+            array.push(random(5,800));
+        }
+        this.setState({array});
+    }
+
+    render(){
+        const {array} = this.state;
+        return(
+            <div className='array-container'>
+                {array.map((value, idx) => (
+                    <div className='array-bar' key={idx} style={{height:`${value}px`}}>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random()*(max-min+1) + min);
+}
